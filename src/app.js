e7c5b38812e422039a15531c3d279a6b0f8d37c3
Márcia Martins <marciammart@gmail.com>
2022-06-16 13:57:50 +0100

    Enabled CORS

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const swaggerUI = require("swagger-ui-express");
 const swaggerDocs = require("./swagger.json");
 
 const port = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors());
 app.use("/api", require("./api"));
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
